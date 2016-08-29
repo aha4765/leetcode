e200668e6bfa03939cd7e64510bf4c77739bcd60
aha4765 <liufl12@qq.com>
2016-08-28 22:05:25 -0400

    Create 62.cpp

---
+++ 62.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> table(m, vector<int>(n, 1));
+        if ((m == 0) && (n == 0)) return 1;
+        
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j) {
+                table[i][j] = table[i-1][j] + table[i][j-1];
+            }
+        }
+        return table[m-1][n-1];
+    }
+};
