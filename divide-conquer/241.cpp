cb5399bc59d5358034ca2938f549b393b1ea2555
aha4765 <liufl12@qq.com>
2016-08-30 19:27:28 -0400

    Create 241.cpp

---
+++ divide-conquer/241.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> diffWaysToCompute(string input) {
+        vector<int> res;
+        int pos = -1;
+        while ((pos = input.find('+', pos+1)) != string::npos) {
+            for (auto temp1 : diffWaysToCompute(input.substr(0, pos))) {
+                for (auto temp2 : diffWaysToCompute(input.substr(pos+1, input.size()-1-pos))) {
+                    res.push_back(temp1 + temp2);
+                }
+            }
+        }
+        pos = -1;
+        while ((pos = input.find('-', pos+1)) != string::npos) {
+            for (auto temp1 : diffWaysToCompute(input.substr(0, pos))) {
+                for (auto temp2 : diffWaysToCompute(input.substr(pos+1, input.size()-1-pos))) {
+                    res.push_back(temp1 - temp2);
+                }
+            }
+        }
+        pos = -1;
+        while ((pos = input.find('*', pos+1)) != string::npos) {
+            for (auto temp1 : diffWaysToCompute(input.substr(0, pos))) {
+                for (auto temp2 : diffWaysToCompute(input.substr(pos+1, input.size()-1-pos))) {
+                    res.push_back(temp1 * temp2);
+                }
+            }
+        }
+        if (res.size() == 0) {
+            res.push_back(stoi(input));
+        }
+        return res;
+    }
+};
