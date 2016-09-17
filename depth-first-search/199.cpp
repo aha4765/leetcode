6081fafa72444bd131da9dd9f4609c90eb51e8f5
aha4765 <liufl12@qq.com>
2016-09-17 19:03:19 -0400

    Create 199.cpp

---
+++ depth-first-search/199.cpp
@@ -0,0 +1,46 @@
+// This is the solution with BFS
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+ // first try bfs
+class Solution {
+public:
+    struct DataSet {
+        TreeNode* node_;
+        int level_;
+        
+        DataSet(TreeNode* node, int level) {
+            node_ = node;
+            level_ = level;
+        }
+    };
+    
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> res;
+        if (root == nullptr) return res;
+        queue<DataSet> visited;
+        visited.push(DataSet(root, 0));
+        int curlevel = -1;
+        while (!visited.empty()) {
+            DataSet cur = visited.front();
+            visited.pop();
+            if (curlevel < cur.level_) {
+                res.push_back(cur.node_->val);
+                curlevel = cur.level_;
+            }
+            if (cur.node_->right != nullptr) {
+                visited.push(DataSet(cur.node_->right, cur.level_+1));
+            }
+            if (cur.node_->left != nullptr) {
+                visited.push(DataSet(cur.node_->left, cur.level_+1));
+            }
+        }
+        return res;
+    }
+};
