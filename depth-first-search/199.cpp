734175790f9e98cbd8dc0ea4c25dddb2eef8b41b
aha4765 <liufl12@qq.com>
2016-09-17 19:13:52 -0400

    Update 199.cpp

--- depth-first-search/199.cpp
+++ depth-first-search/199.cpp
@@ -44,3 +44,35 @@ public:
         return res;
     }
 };
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+ // then try dfs
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> res;
+        if (root == nullptr) return res;
+        dfsHelper(root, 0, res);
+        return res;
+    }
+    int dfsHelper(TreeNode* node, int level, vector<int>& res) {
+        if (level >= res.size()) {
+            res.push_back(node->val);
+        }
+        if (node->right != nullptr) {
+            dfsHelper(node->right, level+1, res);
+        }
+        if (node->left != nullptr) {
+            dfsHelper(node->left, level+1, res);
+        }
+        return 0;
+    }
+};
