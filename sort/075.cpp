df66b982bd8bffd250d2ca0ac3ec4c7987002580
aha4765 <liufl12@qq.com>
2016-10-25 21:28:20 -0400

    Create 075.cpp

---
+++ sort/075.cpp
@@ -0,0 +1,25 @@
+// This is the three-way partition, which is frequently used in quick sort.
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int i = 0;
+        int j = 0; 
+        int k = nums.size()-1;
+        
+        while (j <= k) {
+            if (nums[j] == 0) {
+                swap(nums[i], nums[j]);
+                ++i;
+                ++j;
+            }
+            else if (nums[j] == 1) {
+                ++j;
+            }
+            else {
+                swap(nums[j], nums[k]);
+                --k;
+            }
+        }
+    }
+};
