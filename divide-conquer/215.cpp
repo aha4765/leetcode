321965944071d062ddb9ecc826b981282cefe55e
aha4765 <liufl12@qq.com>
2016-10-31 21:44:34 -0400

    Update 215.cpp

--- divide-conquer/215.cpp
+++ divide-conquer/215.cpp
@@ -42,3 +42,41 @@ public:
         return j;
     }
 };
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        int pos = 0;
+        int start = 0;
+        int end = nums.size()-1;
+        while ((pos = partition(nums, start, end)) != (k-1)) {
+            if (pos < k-1) {
+                start = pos+1;
+            }
+            else {
+                end = pos-1;
+            }
+        }
+        return nums[k-1];
+    }
+    
+    int partition(vector<int>& nums, int start, int end) {
+        if (start == end) return start;
+        int cur = start;
+        int pivot = nums[start];
+        start++;
+        while (start <= end) {
+            while ((nums[start] >= pivot) && (start <= end)) {
+                start++;
+            }
+            while ((nums[end] < pivot) && (start <= end)) {
+                end--;
+            }
+            if (start < end) {
+                swap(nums[start++], nums[end--]);
+            }
+        }
+        swap(nums[cur], nums[end]);
+        return end;
+    }
+};
