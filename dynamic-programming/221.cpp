53b0c4488a4c57f2f4e92760c317442dbe7abba5
aha4765 <liufl12@qq.com>
2016-10-31 21:03:29 -0400

    Update 221.cpp

--- dynamic-programming/221.cpp
+++ dynamic-programming/221.cpp
@@ -48,3 +48,37 @@ public:
         return size*size;
     }
 };
+
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        if ((matrix.size() == 0) || (matrix[0].size() == 0)) return 0;
+        int nx = matrix.size();
+        int ny = matrix[0].size();
+        
+        vector<vector<int>> size(nx, vector<int>(ny, 0));
+        int maxsq = 0;
+        
+        for (int i = 0; i < ny; ++i) {
+            size[0][i] = (matrix[0][i] == '1') ? 1 : 0;
+            maxsq = max(size[0][i], maxsq);
+        }
+        for (int i = 0; i < nx; ++i) {
+            size[i][0] = (matrix[i][0] == '1') ? 1 : 0;
+            maxsq = max(size[i][0], maxsq);
+        }
+        
+        for (int i = 1; i < nx; ++i) {
+            for (int j = 1; j < ny; ++j) {
+                if (matrix[i][j] == '1') {
+                    size[i][j] = min(min(size[i-1][j], size[i][j-1]), size[i-1][j-1]) + 1;
+                    maxsq = max(size[i][j], maxsq);
+                }
+                else {
+                    size[i][j] = 0;
+                }
+            }
+        }
+        return maxsq*maxsq;
+    }
+};
